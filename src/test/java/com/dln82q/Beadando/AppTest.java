079c57483013d0655cf6a21df131d20740b7b495
Kiss68 <72266991+Kiss68@users.noreply.github.com>
2020-12-03 23:30:09 +0100

    Add files via upload

---
+++ src/test/java/com/dln82q/Beadando/AppTest.java
@@ -0,0 +1,20 @@
+package com.dln82q.Beadando;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
